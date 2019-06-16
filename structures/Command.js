402881f7e5701ec9d7cf1d9db2aba36443d2e5f3
Loliticos <47262825+Loliticos@users.noreply.github.com>
2019-06-16 18:01:39 -0300

    Update Command.js

--- structures/Command.js
+++ structures/Command.js
@@ -22,7 +22,6 @@ module.exports = class Command {
        };
    }
    runCommand(message, prefix, args){
-       let _this = this
        try {
        const client = message.guild.me
        if(options.permissions) {
@@ -34,33 +33,11 @@ module.exports = class Command {
 	  }
 	       
        }
-       if(!client.hasPermissions(options.permissions))
 	
-       this.run({
-           message:message,
-           args: args,
-           client: message.client,
-           prefix:prefix,
-		   buildMessage(params={}){
-            params.title  = params.title ? params.title : _this.name.pop();
-            params.color = params.color || message.client.color;
-            params.footer = params.footer ||  {
-              icon_url:message.author.displayAvatarURL,
-              text:message.author.username
-            }
-            params = {embed:params};
-             return {
-                 send(id){
-                     if (!id) message.channel.send(params)
-                     else message.client.channels.get(id.toString()).send(params)
-                 }
-             }}
-       })}
-       catch(err){
-         if (message.client.logErrors)  console.error(`${err.toString()} in command: ${this.name.pop()}`);
-         message.client.emit("commandError",_this,err);
-       }
-       this.ob.push(message.author.id)
-       setTimeout(() => this.ob.splice(this.ob.indexOf(message.authorid),1),1000*this.cd)
+       const promise = this.run(message, args, client, prefix)
+       
+       const retVal = await promise
+       
+       return retVal
   }
 }
